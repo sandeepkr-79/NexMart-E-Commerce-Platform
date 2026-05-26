f17308934e0f4ff9e87698c1719cd6705719ac75
sandeepkr-79 <user.email "sk7643224@gmail.com>
2026-05-26 22:48:02 +0530

    port changed

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -12,7 +12,7 @@ let transporter = null;
 if (isEmailConfigured) {
   transporter = nodemailer.createTransport({
     host: 'smtp-relay.brevo.com',
-    port: 587,
+    port: 2525,
     secure: false,
 
     auth: {
