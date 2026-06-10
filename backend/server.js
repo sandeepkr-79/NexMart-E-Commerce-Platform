9f0c03d93c556c2bc7fcb5dc39fa2a7b247f17f6
sandeepkr-79 <user.email "sk7643224@gmail.com>
2026-06-10 07:49:33 +0530

    Fix CORS configuration

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,8 @@ app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
 app.use(xss());
 
 const corsOptions = {
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  // origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: "https://nex-mart-e-commerce-platform.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
